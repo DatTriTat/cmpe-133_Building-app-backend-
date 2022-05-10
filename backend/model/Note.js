3dd8baa78ba9c0222301ff1094e4e79c8e3e26ff
Dat Tri Tat <tatdattri0306@gmail.com>
2022-05-10 14:03:50 -0700

    update

--- backend/model/Note.js
+++ backend/model/Note.js
@@ -3,18 +3,40 @@ const Schema = mongoose.Schema;
 
 
 const noteSchema = new Schema({
-    title: {
+    username: {
         type: String,
+        required: true
     },
-    content: {
-        type: String,
+    age: {
+        type: Number,
+        required: true
     },
 
-    user:{
-        type: mongoose.Schema.Types.ObjectId, ref: 'User'
-    }
+    phone: {
+        type: String,
+        required: true
+    },
 
-    
+    email: {
+        type: String,
+        required: true
+    },
+
+    date: {
+        type: Date,
+        required: true
+    },
+
+    time: {
+        type: String,
+        required: true
+    },
+
+    description: {
+        type: String,
+        required: true
+    },
+
 });
 
 module.exports = mongoose.model('Notes', noteSchema);
